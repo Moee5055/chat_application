e3c0d3660ef23d900cbd157321ab92e43fbb7cca
Moee5055 <kshitij.moee55@gmail.com>
2025-06-20 21:50:53 +0545

    configure shacn and tailwindcss on project
    configure shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
-  return <h1>hello</h1>;
+  return (
+    <>
+      <h2>First page</h2>
+      <Button className="bg-red-500 text-white p-5 mt-5">Hello</Button>
+    </>
+  );
 }
