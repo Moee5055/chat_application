d10e4e322f8f3e1395f03a5685db4d7267edd8c2
Moee5055 <kshitij.moee55@gmail.com>
2025-06-21 16:46:02 +0545

    routing added; create index route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from "react-router";
 import IndexPage from "./routes/index/IndexPage";
 
 export default function App() {
   return (
-    <div>
-      <IndexPage />
-    </div>
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
+      <Route
+        path="/auth/login"
+        element={<div>Hello this is the auth path</div>}
+      />
+    </Routes>
   );
 }
