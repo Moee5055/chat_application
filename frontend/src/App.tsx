c44825b505c7e2825972bf49a5b459dc606f0396
Moee5055 <kshitij.moee55@gmail.com>
2025-06-21 17:38:53 +0545

    added login route and login form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router";
 import IndexPage from "./routes/index/IndexPage";
+import LoginPage from "./routes/auth/login/LoginPage";
+import AuthLayout from "./routes/auth/AuthLayout";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<IndexPage />} />
-      <Route
-        path="/auth/login"
-        element={<div>Hello this is the auth path</div>}
-      />
+      <Route path="auth" element={<AuthLayout />}>
+        <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<div>Sign up element</div>} />
+      </Route>
     </Routes>
   );
 }
--- frontend/src/routes/auth/login/LoginPage.tsx
+++ frontend/src/routes/auth/login/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { LoginForm } from "@/components/login-form";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return <LoginForm className="min-w-sm max-w-md" />;
 }
