cfdb79d8197eb47ebf229e4a7e86ec31e422a89b
Moee5055 <kshitij.moee55@gmail.com>
2025-06-26 00:20:52 +0545

    added link on login and signup button on landingpage

--- frontend/src/routes/index/components/LandingPageContent.tsx
+++ frontend/src/routes/index/components/LandingPageContent.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { LogIn, User } from "lucide-react";
+import { useNavigate } from "react-router";
 
 export default function LandingPageContent() {
+  const navigate = useNavigate();
   return (
     <div className="px-6 flex items-center">
-      <div className="mt-[-5rem]">
+      <div className="mt-[-6rem] px-7">
         <div className="py-3">
           <h1 className="text-5xl font-bold text-[#1E293B]">
             Welcome to ChatWave
@@ -12,11 +14,21 @@ export default function LandingPageContent() {
           <p className="text-sm text-[#1E293B]/80 font-semibold">{`Connect Chat Collaborate - anytime, anywhere.`}</p>
         </div>
         <div className="mt-2 space-x-4 flex items-center">
-          <Button className="bg-[#3B82F6] cursor-pointer shadow-lg transition-all hover:transition-shadow hover:bg-blue-700 w-[110px]">
+          <Button
+            className="bg-[#3B82F6] cursor-pointer shadow-lg transition-all hover:transition-shadow hover:bg-blue-700 w-[110px]"
+            onClick={() => {
+              navigate("/auth/login");
+            }}
+          >
             <LogIn />
             Login
           </Button>
-          <Button className="bg-[#3B82F6] cursor-pointer shadow-lg transition-all hover:transition-shadow hover:bg-blue-700 w-[110px]">
+          <Button
+            className="bg-[#3B82F6] cursor-pointer shadow-lg transition-all hover:transition-shadow hover:bg-blue-700 w-[110px]"
+            onClick={() => {
+              navigate("/auth/signup");
+            }}
+          >
             <User />
             SignUp
           </Button>
